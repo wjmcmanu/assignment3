fec9ea1bc4d499086f2c2daece2ccf1be301b6fd
Jamie McManus <jamie@wjm.ca>
2019-10-05 20:12:08 -0400

    Added the first bit of code to test for the first example only

--- prime.py
+++ prime.py
@@ -1,3 +1,9 @@
 """
 prime.py -- Write the application code here
 """
+def generate_prime_factors(number):
+    """
+    Will accept an int and will return a list of ints
+    """
+    if not isinstance(number, int):
+        raise ValueError
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -1,4 +1,13 @@
 # pylint: disable=missing-docstring
+import pytest
+import prime
 """
 The test module for Prime Factors
 """
+def test_not_int():
+    """
+    This will check to see if the value passed
+    in is an int otherwise throw an Error
+    """
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors("a")
