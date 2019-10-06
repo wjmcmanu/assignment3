659250605ffe3dde481e942ef73d978b0dfe3882
Jamie McManus <jamie@wjm.ca>
2019-10-05 20:28:00 -0400

    Removed the comments at the top in an effort to pass the pylint check

--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -1,13 +1,10 @@
 # pylint: disable=missing-docstring
 import pytest
 import prime
-"""
-The test module for Prime Factors
-"""
+
 def test_not_int():
     """
-    This will check to see if the value passed
-    in is an int otherwise throw an Error
+    test_not_int() - test to see if the value is an int or not
     """
     with pytest.raises(ValueError):
         prime.generate_prime_factors("a")
