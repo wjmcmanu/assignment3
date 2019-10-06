8bad6e37da6802cc293118f748884852b9fd6029
Jamie McManus <jamie@wjm.ca>
2019-10-05 20:35:10 -0400

    I've added the module docstring back in

--- prime.py
+++ prime.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+""" prime.py -- This program will generate a list of prime numbers for a
+given number"""
+
 def generate_prime_factors(number):
     """
-    Will accept an int and will return a list of ints
+    Will accept an int and will return a list of
     """
     if not isinstance(number, int):
         raise ValueError
